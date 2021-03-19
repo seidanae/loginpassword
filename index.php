a542f6d431254cdb064fb4e14dcdf8c100e55533
seidanae <80508358+seidanae@users.noreply.github.com>
2021-03-19 09:19:23 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>Login</title>
+</head>
+<form action="trylog.php"method="post">
+<label for="username">Username</label>
+<input type="username" id="usename" name="username"><br/><br/>
+<label for ="password">Password:</label>
+<input type="text" id="password" name="password"><br/><br/>
+<button type="submit"><a href ="https://seidanasprofilee.herokuapp.com/">login</a></button>
+<button type= "reset">clear</input>
+</form>
+</html>
